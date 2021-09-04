2c687df262dababc28463c44097f316586395d7a
thierno sadou diallo <thierved01@gmail.com>
2021-09-04 16:43:06 +0000

    learned routing

--- go.mod
+++ go.mod
@@ -1,3 +1,16 @@
 module go-web
 
 go 1.17
+
+require (
+	github.com/cosmtrek/air v1.27.3 // indirect
+	github.com/creack/pty v1.1.15 // indirect
+	github.com/fatih/color v1.12.0 // indirect
+	github.com/fsnotify/fsnotify v1.5.1 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/imdario/mergo v0.3.12 // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/mattn/go-isatty v0.0.13 // indirect
+	github.com/pelletier/go-toml v1.9.3 // indirect
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+)
--- main.go
+++ main.go
@@ -2,15 +2,23 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
 	"net/http"
 )
 
-
 func handleHome(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Welcome to my page!<h1>")
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Welcome to my new page!</h1>")
+
+}
+
+func handleAbout(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<p>More about us?</p>")
 }
 
 func main() {
-	http.HandleFunc("/", handleHome)
-	http.ListenAndServe(":3000", nil)
+	r := mux.NewRouter()
+	r.HandleFunc("/", handleHome)
+	r.HandleFunc("/about", handleAbout)
+	http.ListenAndServe(":3000", r)
 }
